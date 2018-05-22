06bfdabfc5e4df71605d4c0a22160cba3fb41757
ashok2793 <38404064+ashok2793@users.noreply.github.com>
2018-05-22 05:17:39 -0400

    Update ServiceClass.java

--- database-services/src/main/java/com/ibm/ServiceClass.java
+++ database-services/src/main/java/com/ibm/ServiceClass.java
@@ -10,6 +10,8 @@ public class ServiceClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world");
+		
+		System.out.println("Hello world");
 		SpringApplication.run(ServiceClass.class,args);
 	}
 
